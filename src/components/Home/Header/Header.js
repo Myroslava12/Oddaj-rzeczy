9e1480546bf2ed9781536cf2a1065fbba7192113
Myroslava <myroslavaskala@gmail.com>
2020-11-02 20:01:01 +0100

    Add style components Navigation and Header

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -10,10 +10,20 @@ const Header = () => {
             <div className="header--section">
                 <h1 className="header--title">Zacznij pomagać!</h1>
                 <h1 className="header--title">Oddaj niechciane rzeczy w zaufane ręce</h1>
-                <img src={decoration} alt="decoration" />
+                <img className="header--img" src={decoration} alt="decoration" />
                 <div className="header--links--box">
-                    <Link to={ROUTES.LOG_IN} className="header--link link--give">Oddaj rzeczy</Link>
-                    <Link to={ROUTES.LOG_IN} className="header--link link--organize">Zorganizuj zbiórkę</Link>
+                    <Link to={ROUTES.LOG_IN} className="header--link link--give">
+                        <div>
+                            <span>Oddaj </span> <br />
+                            <span>rzeczy</span>
+                        </div>
+                    </Link>
+                    <Link to={ROUTES.LOG_IN} className="header--link link--organize">
+                        <div>
+                            <span>Zorganizuj </span> <br />
+                            <span>zbiórkę</span>
+                        </div>
+                    </Link>
                 </div>
             </div>      
         </div>
--- src/components/Home/Navigation/Navigation.js
+++ src/components/Home/Navigation/Navigation.js
@@ -7,15 +7,15 @@ const Navigation = () => {
     return (
         <nav className="nav--box">
             <ul className="nav--links">
-                <li><NavLink exact to={ROUTES.LOG_IN} >Zaloguj</NavLink></li>
-                <li><NavLink to={ROUTES.REGISTER} ClassName="active--link">Załóż konto</NavLink></li>
+                <li><NavLink exact to={ROUTES.LOG_IN} className="link--auth">Zaloguj</NavLink></li>
+                <li><NavLink to={ROUTES.REGISTER} className="link--auth link--auth--active" >Załóż konto</NavLink></li>
             </ul>
             <ul className="nav--links">
-                <li><Link to="home" smooth={true} duration={100}>Start</Link></li>
-                <li><Link to="description" smooth={true} duration={300}>O co chodzi?</Link></li>
-                <li><Link to="aboutUs" smooth={true} duration={500}>O nas</Link></li>
-                <li><Link to="organizations" smooth={true} duration={600}>Fundacja i organizacje</Link></li>
-                <li><Link to="contact" smooth={true} duration={1000}>Kontact</Link></li>
+                <li><Link to="home" smooth={true} duration={100} className="link--home link--home--active" >Start</Link></li>
+                <li><Link to="description" smooth={true} duration={300} className="link--home" >O co chodzi?</Link></li>
+                <li><Link to="aboutUs" smooth={true} duration={500} className="link--home" >O nas</Link></li>
+                <li><Link to="organizations" smooth={true} duration={600} className="link--home" >Fundacja i organizacje</Link></li>
+                <li><Link to="contact" smooth={true} duration={1000} className="link--home" >Kontact</Link></li>
             </ul>
         </nav>
     )
